1d2787c3e831bb99e5d5d078b5933d2ed90f9155
Luke Davis <luke@lukealexdavis.co.uk>
2023-08-29 20:05:57 +0100

    new rss?

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,25 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import site from '../data/site.json';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function get(context) {
 	const posts = await getCollection('blog');
+	const allPosts = Object.values(posts).sort(
+	  (a, b) =>
+	    new Date(b.pubDate).valueOf() -
+	    new Date(a.pubDate).valueOf()
+	);
 	return rss({
 		title: site.title,
 		description: site.description,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
+		items: allPosts.map((post) => ({
 			link: `/blog/${post.slug}/`,
-		})),
-	});
-}
+			content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
+    }))
+		});
+	}

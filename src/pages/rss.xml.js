e72e55dd6c8feebc45ef89e854eae0263b787966
Luke Davis <luke@lukealexdavis.co.uk>
2023-08-30 00:20:59 +0100

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import site from '../data/site.json';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function get(context) {
 	const posts = await getCollection('blog');
@@ -18,7 +15,6 @@ export async function get(context) {
 		site: context.site,
 		items: allPosts.map((post) => ({
 			link: `/blog/${post.slug}/`,
-			content: sanitizeHtml(parser.render(post.body)),
       ...post.data,
     }))
 		});

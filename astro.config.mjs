57d3b3824e0db86458b5dde31a738972ee3491cc
Luke Davis <luke@lukealexdavis.co.uk>
2025-09-14 21:33:00 +0100

    updated config + lastfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+	server: { port: 4322 },
 	site: 'https://centiskor.ch',
 	integrations: [
 		mdx(),
